d2458bf7c72f675fb8cf1b909215d045713a8cae
Olivier Girardot <olive_girardot@hotmail.com>
2023-05-20 16:22:33 +0200

    implemented open ai gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
+  require "openai"
+
   def home
+  end
+  
+  def ask
+    client = OpenAI::Client.new(api_key: ENV["OPEN_AI_KEY"])
+
+    response = client.completions(
+      engine: "text-davinci-003",
+      prompt: params[:query],
+      temperature: 0.7,
+      max_tokens: 250,
+      n: 1,
+      stop: nil
+    )
+
+    @response = response.choices[0].text
   end
 end
